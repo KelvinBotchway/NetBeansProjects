f08e9d189306eb4ee805b5ce82a494acf5b83b1e
Kelvin Botchway <Kelvinjay1000@gmail.com>
2022-08-17 08:22:58 +0000

    Lecture 7
    JavaFx Example

---
+++ MyJavaFxExample/src/BidirectionalBindingDemo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author George Kodjo Anni
+ */
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class BidirectionalBindingDemo {
+  public static void main(String[] args) {       
+    DoubleProperty d1 = new SimpleDoubleProperty(1);
+    DoubleProperty d2 = new SimpleDoubleProperty(2);
+    d1.bindBidirectional(d2);
+    System.out.println("d1 is " + d1.getValue() 
+      + " and d2 is " + d2.getValue());
+    d1.setValue(50.1);
+    System.out.println("d1 is " + d1.getValue() 
+      + " and d2 is " + d2.getValue());
+    d2.setValue(70.2);
+    System.out.println("d1 is " + d1.getValue() 
+      + " and d2 is " + d2.getValue());
+  }
+}
